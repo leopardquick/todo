04515d1a63ce27d02de959132d45b90b54460b15
unknown <nadirhemed@gmail.com>
2020-07-01 18:39:46 +0300

    complete ejs initial setup

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
-app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(express.static("public"))
+app.set("view engine","ejs")
 
 
-app.get("/",(req,res)=>{
-      res.send("working with es3")
+app.get("/", (req, res) => {
+
+  let data = new Date()
+  let currentDay = data.getDay()
+  let day = ""
+
+  if(currentDay == 0 || currentDay == 6 )day = "its weekend"
+  else day = "its weekday"
+
+
+  res.render("index" ,{ todayDate:day})
 })
-app.listen(3000,()=>{
+app.listen(3000, () => {
   console.log("listening at port 3000")
 })
